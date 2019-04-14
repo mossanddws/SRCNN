8ee616f133a95954f708661c28f4500d30c32f89
MO Fei <fei.mo@efrei.net>
2019-04-14 11:44:30 +0200

    20190414

---
+++ src/SRCNN/temp.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+input_x = np.asanyarray(
+    [[(1, 2, 3, 4), (5, 6, 7, 8), (1, 2, 3, 4)], [(9, 10, 11, 12), (12, 14, 15, 16), (9, 10, 11, 12)],
+     [(17, 18, 19, 20), (21, 22, 23, 24), (17, 18, 19, 20)]])
+print(input_x)
+print(np.shape(input_x))
+# input_X = np.hsplit(input_x, 3)
+# print(input_X)
